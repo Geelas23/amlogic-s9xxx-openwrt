c80e92b1856cebac41be98d5fa3201afb9b00c2e
Geelas23 <fencilong@proton.me>
2026-08-27 22:34:46 +0700

    Clean up sed commands in diy-part2.sh
    Removed redundant sed commands for package removal.

--- config/openwrt_main/diy-part2.sh
+++ config/openwrt_main/diy-part2.sh
@@ -30,11 +30,6 @@ echo "DISTRIB_SOURCEBRANCH='main'" >>package/base-files/files/etc/openwrt_releas
 # Remove existing ccache settings
 sed -i '/CONFIG_DEVEL/d' .config
 sed -i '/CONFIG_CCACHE/d' .config
-sed -i '/bmx7/d' .config
-sed -i '/babeld/d' .config
-sed -i '/dcwapd/d' .config
-sed -i '/prometheus-node-exporter-lua/d' .config
-sed -i '/kmod-dummy/d' .config
 # Apply new ccache configuration
 if [[ "${2}" == "true" ]]; then
     echo "CONFIG_DEVEL=y" >>.config
@@ -45,7 +40,11 @@ else
     echo "# CONFIG_CCACHE is not set" >>.config
     echo 'CONFIG_CCACHE_DIR=""' >>.config
 fi
-#
+sed -i '/bmx7/d' .config
+sed -i '/babeld/d' .config
+sed -i '/dcwapd/d' .config
+sed -i '/prometheus-node-exporter-lua/d' .config
+sed -i '/kmod-dummy/d' .config
 # ------------------------------- Main source configuration ends -------------------------------
 
 # ------------------------------- Additional customizations -------------------------------
